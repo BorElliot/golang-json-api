8feceec97400e0648dff4bac182d2b08c30547fd
arno <arno.xu@etocrm.com>
2018-10-18 12:51:47 +0800

    get single article

--- main.go
+++ main.go
@@ -1,14 +1,62 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
+type Article struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Homepage.")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{ID: 1, Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{ID: 2, Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	articles := Articles{
+		Article{ID: 1, Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{ID: 2, Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+
+	for _, v := range articles {
+		if v.ID == id {
+			json.NewEncoder(w).Encode(v)
+		}
+	}
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
 func main() {
-
-	images := http.FileServer(http.Dir("assets"))
-	http.Handle("/assets/", http.StripPrefix("/assets/", images))
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	handleRequests()
 }
